d6bdb965c17404c1eff451524e483abb3e5b5a22
DESKTOP-P6QN5A2\admin <2097620373@qq.com>
2020-11-20 14:44:50 +0800

    删除

--- wz/src/components/HelloWorld.vue
+++ wz/src/components/HelloWorld.vue
@@ -1,50 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>牛皮</h2>
-    <h2>厉害</h2>
-    <h3>哈哈哈哈</h3>
-    <h3>33333333333</h3>
-    <h3>绅士</h3>
-    <h3>4444444444444</h3>
-    <span>wwwwwwwwwww</span>
-    <p>呵呵呵呵呵呵</p>
-    <span>吴振</span>
-    <span>吴振</span>
-    <span>吴振</span>
-    <div>蒋腾蛟哈哈哈1111</div>
-
-
-    <div>蒋腾蛟哈哈哈1111</div>
-    <div>蒋腾蛟哈哈哈1111</div>
-    <div>蒋腾蛟哈哈哈1111</div>
-
-    <span>吴振1111</span>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+      吴振
   </div>
 </template>
 
